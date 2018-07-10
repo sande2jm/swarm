decd00c3c0c42b7de9195f4d8d37c5a909793bda
sande2jm <sande2jm@gmail.com>
2018-07-10 01:01:19 -0400

    version 6.5

--- test/new_swarm_launch.py
+++ test/new_swarm_launch.py
@@ -10,12 +10,19 @@ direc = config['worker_direc_name']
 github_clone = 'git clone https://github.com/'+ config['github_account'] + '/' + direc + '.git'
 rm_repo = 'sudo rm -r ' + direc
 launch = config['launch']
-aws_resources = config['aws_resources']
+aws_services_info = config['aws_services_info']
+
+
 ec2_config = config['ec2_config']
 
 statics = config['statics']
 variables = config['variables']
 splits = config['splits']
+
+s3_info = aws_services_info['s3']
+s3 = boto3.resource('s3')
+
+s3.Bucket(s3_info['bucket']).download_file(splits['filename'], 'swarm/test/train.json')
 
 size = ec2_config['size']
 swarm_name = ec2_config['name']
@@ -25,9 +32,9 @@ swarm.init(dependencies=ec2_config['dependencies'])
 swarm.populate()
 swarm.describe()
 
-# json_input = mpu.io.read('swarm/test/train.json')
-# splits = ({'images':json_input['images']})
-# variables = ({'index': ((0,size), 'unique')})
+json_input = mpu.io.read('swarm/test/'+splits['filename'])
+splits = ({'images':json_input['images']})
+variables = ({'index': ((0,size), 'unique')})
 
 hive = Hive3(variable=variables, static=statics, split=splits, direc=direc)
 hive.gather(size=size,group=swarm_name)
