8a56c6d17b7712dd20ed8c0119907a363a085bb2
sande2jm <sande2jm@gmail.com>
2018-07-09 20:38:12 -0400

    version 4.2

--- core/hive3.py
+++ core/hive3.py
@@ -68,7 +68,7 @@ class Hive3():
 				f.write(self.state[0])
 			for x,params in self.swarm.items():
 				state_scp = 'scp -i swarm/DLNAkey.pem state.txt ubuntu@'+ params['public_dns_name']+':worker/'
-				call(scp_pem_key.split(" "))
+				call(state_scp.split(" "))
 		print("Done")
 
 	def display(self):
